acd4ccc721e33bb8d2e6f50930bbeecf7abf2d86
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2023-03-24 14:53:23 +0000

    Gravity now affects the player and nearby objects

--- Assets/Scripts/Player/Gravity.cs
+++ Assets/Scripts/Player/Gravity.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 namespace Player {
 	public class Gravity : MonoBehaviour {
+		[SerializeField] private GameObject m_areaObject;
+
+		[HideInInspector] public LayerMask Layer { get; private set; }
+		[HideInInspector] public float AmountPerTick { get; private set; } = Physics.gravity.y / 50;
+		[HideInInspector] public Vector3 AccelerationPerTick { get; private set; }
+
+		public Gravity() {
+			AccelerationPerTick = new Vector3(0, -AmountPerTick, 0);
+		}
 		private void Awake() {
+			Layer = m_areaObject.layer;
 			Globals.CurrentGravityController = this;
 		}
 	}
--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -31,18 +31,18 @@ namespace Player {
 			MoveTick(ref vel);
 			JumpTick(ref vel);
 			CapVelocity(ref vel);
-			rb.velocity = vel;
 
-			GravityTick();
+			GravityTick(ref vel);
+			rb.velocity = vel;
 		}
 
 
 		private void MoveTick(ref Vector3 vel) {
-            float rad = Mathf.Atan2(moveInput.x, moveInput.y) + (cam.transform.eulerAngles.y * Mathf.Deg2Rad);
-            float distance = Mathf.Sqrt(Mathf.Pow(moveInput.x, 2) + Mathf.Pow(moveInput.y, 2));
-            vel.x += Mathf.Sin(rad) * distance * m_moveData.acceleration;
-            vel.z += Mathf.Cos(rad) * distance * m_moveData.acceleration;
-        }
+			float rad = Mathf.Atan2(moveInput.x, moveInput.y) + (cam.transform.eulerAngles.y * Mathf.Deg2Rad);
+			float distance = Mathf.Sqrt(Mathf.Pow(moveInput.x, 2) + Mathf.Pow(moveInput.y, 2));
+			vel.x += Mathf.Sin(rad) * distance * m_moveData.acceleration;
+			vel.z += Mathf.Cos(rad) * distance * m_moveData.acceleration;
+		}
 		private void JumpTick(ref Vector3 vel) {
 			if (jumpInput) {
 				vel.y += m_moveData.jumpPower;
@@ -50,12 +50,12 @@ namespace Player {
 			}
 		}
 		private void CapVelocity(ref Vector3 vel) {
-            vel.x = Util.AbsMax(vel.x, m_moveData.maxSpeed);
-            vel.z = Util.AbsMax(vel.z, m_moveData.maxSpeed);
-        }
+			vel.x = Util.AbsMax(vel.x, m_moveData.maxSpeed);
+			vel.z = Util.AbsMax(vel.z, m_moveData.maxSpeed);
+		}
 
-		private void GravityTick() {
-
+		private void GravityTick(ref Vector3 vel) {
+			vel += Globals.CurrentGravityController.AccelerationPerTick;
 		}
 	}
 }
