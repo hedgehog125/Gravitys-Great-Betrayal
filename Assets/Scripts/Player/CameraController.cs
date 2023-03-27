e481e3a9ca57779b3d3c2d4b726f13eaf6c3f928
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2023-03-27 12:20:34 +0100

    Started trying to add the silhouette effect

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -7,6 +7,9 @@ namespace Player {
 	public class CameraController : MonoBehaviour {
 		[SerializeField] private CinemachineFreeLook m_defaultVCam;
 
+		private void Awake() {
+			Cursor.lockState = CursorLockMode.Locked;
+		}
 		private void Start() {
 			m_defaultVCam.Follow = Globals.CurrentPlayerObject.transform;
 			m_defaultVCam.LookAt = Globals.CurrentPlayerObject.transform;
