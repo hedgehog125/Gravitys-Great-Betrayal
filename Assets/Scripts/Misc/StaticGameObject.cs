3ea2fba2f2c639a5a9bfe33b9e58c5f7f68d33e0
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2023-04-03 22:25:02 +0100

    Got the lighting working how I want it
    I wonder if there's a better way of doing this

---
+++ Assets/Scripts/Misc/StaticGameObject.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class StaticGameObject : MonoBehaviour {
+	[SerializeField] private bool m_bakedShadows = true;
+	[SerializeField] private bool m_realtimeShadows = false;
+
+	private void Awake() {
+		if (! EditorApplication.isPlayingOrWillChangePlaymode) return;
+
+		Renderer ren = GetComponent<Renderer>();
+		ren.shadowCastingMode = m_realtimeShadows? UnityEngine.Rendering.ShadowCastingMode.On : UnityEngine.Rendering.ShadowCastingMode.Off;
+	}
+
+	#if UNITY_EDITOR
+	private void Reset() {
+		gameObject.isStatic = true;
+	}
+
+	private void Update() {
+		if (EditorApplication.isPlayingOrWillChangePlaymode) return;
+
+		Renderer ren = GetComponent<Renderer>();
+		ren.shadowCastingMode = m_bakedShadows? UnityEngine.Rendering.ShadowCastingMode.On : UnityEngine.Rendering.ShadowCastingMode.Off;
+	}
+	#endif
+}
