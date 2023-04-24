069539c3d6dab7ff8b62a9971e534bb41c5d79cf
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2023-04-24 12:14:51 +0100

    Made some progress with fixing the camera

---
+++ Assets/Scripts/Misc/CustomCameraUp.cs
@@ -0,0 +1,21 @@
+// Credit: based on https://forum.unity.com/threads/lookat-center-delay-when-camera-activates.696977/#post-4663325
+
+using UnityEngine;
+using Cinemachine;
+
+/// <summary>
+/// An add-on module for Cinemachine Virtual Camera that overrides Up
+/// </summary>
+[SaveDuringPlay]
+[AddComponentMenu("")] // Hide in menu
+public class CustomCameraUp : CinemachineExtension {
+	public Vector3 Up = new(0, 1, 0);
+
+	protected override void PostPipelineStageCallback(
+		CinemachineVirtualCameraBase vcam,
+		CinemachineCore.Stage stage, ref CameraState state, float deltaTime) {
+		if (stage == CinemachineCore.Stage.Body && Up != null) {
+			state.ReferenceUp = Up;
+		}
+	}
+}
--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -6,9 +6,12 @@ using Cinemachine;
 namespace Player {
 	public class CameraController : MonoBehaviour {
 		[SerializeField] private CinemachineFreeLook m_defaultVCam;
+		[SerializeField] private CustomCameraUp m_cameraUpController;
 
 		private void Awake() {
 			Cursor.lockState = CursorLockMode.Locked;
+
+			m_cameraUpController.Up = new Vector3(0, -1, 0);
 		}
 		private void Start() {
 			m_defaultVCam.Follow = Globals.CurrentPlayer.transform;
