238a1c8165a368a8a989ed1a838f6b2a7337b5f3
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2023-05-16 11:16:47 +0100

    Tried to animate the camera rotating w/ gravity
    Going to use Quaterion.Slerp instead I think

--- Assets/Scripts/Player/Visible.cs
+++ Assets/Scripts/Player/Visible.cs
@@ -6,6 +6,7 @@ namespace Player {
 	public class Visible : MonoBehaviour {
 		[SerializeField] private float m_transparentCameraDistance;
 		[SerializeField] private List<MeshRenderer> m_renderers;
+		[SerializeField] private float rotateSpeed;
 
 		[HideInInspector] public float LookAngle;
 
@@ -13,9 +14,30 @@ namespace Player {
 		private void Awake() {
 			cam = Camera.main;
 		}
+		private void Start() {
+			transform.eulerAngles = Gravity.Rotations[Globals.CurrentGravityController.Direction];
+		}
 
 		private void Update() {
-			transform.eulerAngles = Gravity.Rotations[Globals.CurrentGravityController.Direction];
+			transform.eulerAngles = transform.eulerAngles;
+			Debug.Log(transform.eulerAngles);
+			return;
+			{
+				Vector3 targetRotation = Gravity.Rotations[Globals.CurrentGravityController.Direction];
+				Vector3 rot = transform.eulerAngles;
+				for (int i = 0; i < 3; i++) {
+					float adjusted = (rot[i] + 180) % 360;
+					float diffToTarget = ((targetRotation[i] + 180) % 360) - adjusted;
+
+					rot[i] = Mathf.Abs(diffToTarget) < rotateSpeed?
+						targetRotation[i]
+						: rot[i] + (diffToTarget > 0? rotateSpeed : -rotateSpeed)
+					;
+				}
+				transform.eulerAngles = rot;
+			}
+
+
 			m_renderers[0].transform.localEulerAngles = new Vector3(0, LookAngle, 0);
 
 			if (Vector3.Distance(transform.position, cam.transform.position) < m_transparentCameraDistance) {
