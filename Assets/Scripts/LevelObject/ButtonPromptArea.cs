7e42b93f1f5c028c67f2a15f315c2e7179278274
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2023-06-03 22:26:28 +0100

    Button prompt improvements

--- Assets/Scripts/LevelObject/ButtonPromptArea.cs
+++ Assets/Scripts/LevelObject/ButtonPromptArea.cs
@@ -6,11 +6,31 @@ using UnityEngine;
 namespace LevelObject {
 	public class ButtonPromptArea : MonoBehaviour {
 		[SerializeField] private ButtonPromptData m_prompt;
+		[SerializeField] private int m_delay;
+
+		private bool touchingPlayer;
+		private int activateTick;
 
 		private void OnTriggerStay(Collider collider) {
 			if (collider.gameObject != Globals.CurrentPlayer.gameObject) return;
 
-			Globals.CurrentUIController.ButtonPrompts.Display(m_prompt);
+			touchingPlayer = true;
+		}
+
+		private void FixedUpdate() {
+			if (touchingPlayer) {
+				if (activateTick == m_delay) {
+					Globals.CurrentUIController.ButtonPrompts.Display(m_prompt);
+				}
+				else {
+					activateTick++;
+				}
+			}
+			else {
+				activateTick = 0;
+			}
+
+			touchingPlayer = false;
 		}
 	}
 }
--- Assets/Scripts/UI/ButtonPrompts.cs
+++ Assets/Scripts/UI/ButtonPrompts.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 namespace UI {
 	public class ButtonPrompts : MonoBehaviour {
 		[SerializeField] private int m_showTime;
+		[SerializeField] private int m_animationDuration;
 
 		private int showTick = -1;
+		private int hideAnimationTick = -1;
 		private ButtonPromptData currentPrompt;
+		private ButtonPromptData queuedPrompt;
 		private bool needsRerender;
+		private bool isChangingPrompt;
 
 		private void OnControlSchemeChange() {
 			Render();
@@ -26,16 +30,35 @@ namespace UI {
 		}
 
 		private void FixedUpdate() {
-			if (showTick != -1) {
-				if (needsRerender) {
-					Render();
-					needsRerender = false;
+			if (showTick == -1) {
+				if (hideAnimationTick == m_animationDuration) {
+					hideAnimationTick = -1;
+				}
+				else {
+					hideAnimationTick++;
+				}
+
+				if (hideAnimationTick == -1) {
+					isChangingPrompt = false;
+
+					currentPrompt = queuedPrompt;
+					queuedPrompt = null;
+				}
+			}
+			else {
+				if (!isChangingPrompt) {
+					if (needsRerender) {
+						Render();
+						needsRerender = false;
+					}
 				}
 
 				if (showTick == m_showTime) {
 					anim.SetBool("Show", false);
 					showTick = -1;
 					currentPrompt = null;
+
+					hideAnimationTick = 0;
 				}
 				else {
 					showTick++;
@@ -47,7 +70,6 @@ namespace UI {
 			if (showTick == -1) return;
 
 			int controlSchemeID = Globals.CurrentPlayer.ControlSchemeID;
-			Debug.Log(controlSchemeID);
 			string displayText = "";
 			for (int i = 0; i < currentPrompt.textParts.Count; i++) {
 				displayText += currentPrompt.textParts[i];
@@ -64,10 +86,18 @@ namespace UI {
 		}
 
 		public void Display(ButtonPromptData prompt) {
-			showTick = 0;
-			if (currentPrompt != prompt) {
+			if (! isChangingPrompt) showTick = 0;
+			if (currentPrompt != prompt && queuedPrompt != prompt) {
+				if (currentPrompt == null) {
+					currentPrompt = prompt;
+				}
+				else {
+					showTick = m_showTime;
+					isChangingPrompt = true;
+
+					queuedPrompt = prompt;
+				}
 				needsRerender = true;
-				currentPrompt = prompt;
 			}
 		}
 	}
